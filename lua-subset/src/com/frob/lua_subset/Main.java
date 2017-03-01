98c94fec5be53e0df92f51966dfd0480c49726ec
IDeathI <Tony_Khounxay@yahoo.com>
2017-02-28 20:29:36 -0500

    Update Main.java

--- lua-subset/src/com/frob/lua_subset/Main.java
+++ lua-subset/src/com/frob/lua_subset/Main.java
@@ -1,10 +1,13 @@
 package com.frob.lua_subset;
 
+/*
+ * Main function of the Scanner for test.lua file in detecting tokens. 
+ */
 public class Main {
 
     public static void main(String[] args) {
         System.out.println(args[0]);
-        LexicalAnalyzer scanner = new LexicalAnalyzer();
-        System.out.println(scanner.analyze(args[0]));
+        LexicalAnalyzer scanner = new LexicalAnalyzer(); //Creating the lexeme scanner
+        System.out.println(scanner.analyze(args[0])); //Printing out all the lexemes found
     }
 }
